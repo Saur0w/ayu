125154aefc1dfb9daf545e13e258a2a30bfe9ce3
Saur0w <sthapliyal085@gmail.com>
2026-04-20 19:24:04 +0530

    "Header"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import Landing from "./components/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Landing";
 
-function App() {
 
-  return (
-    <>
-      <Landing />
-    </>
-  )
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    {/*<Route index element={<DashboardPage />} />*/}
+                    {/*<Route path="analytics" element={<AnalyticsPage />} />*/}
+                    {/*<Route path="system-logs" element={<SystemLogsPage />} />*/}
+                    {/*<Route path="settings" element={<SettingsPage />} />*/}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App
--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import styles from "./style.module.css";
+import Sidebar from "../Sidebar/index.jsx";
+import Header from "../Header/index.jsx";
 
 export default function Landing() {
     return (
         <div className={styles.shell}>
+            <Sidebar />
             <div className={styles.main}>
+                <Header />
                 <main className={styles.content}>
                     <Outlet />
                 </main>
