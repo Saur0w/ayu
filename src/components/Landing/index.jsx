c95f06cfd1128247ab85d6f3324e3e70e91ed018
Saur0w <sthapliyal085@gmail.com>
2026-04-20 19:05:19 +0530

    first commit

---
+++ src/components/Landing/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import styles from "./style.module.css";
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className={styles.landing}>
+
+            </div>
+        );
+    }
+}
+
+export default Landing;
